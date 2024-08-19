bb6f9ea2ac9772e904a915c5463c5c04783b7d4c
Garrett Leber <garrett@leberra.com>
2024-08-19 18:21:40 -0400

    improve timer behaviour, lengthen timer

--- clip.go
+++ clip.go
@@ -19,87 +19,86 @@ func tick() tea.Msg {
 type property int
 
 const (
-    copyUsername property = iota
-    copyPassword 
+	copyUsername property = iota
+	copyPassword
 )
 
-
 type UIClip struct {
-    timer int 
-    object string
-    prop property
+	timer  int
+	object string
+	prop   property
 }
 
-func NewUIClip() tea.Model{
-    return UIClip{
-        timer: 10,
-    }
+func NewUIClip() tea.Model {
+	return UIClip{
+		timer: 30,
+	}
 }
 
 func (c UIClip) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
 func (c UIClip) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
-    case ListSelectedEntry:
-        item,ok := msg.item.(BWListItem)
-        if !ok {
-            panic("Could not get BWListItem")
-        }
-        switch msg.prop {
-        case copyPassword:
-            data,err := bwm.GetPassword(item.ID)       
-            if err != nil {
-                panic("Error getting Password")
-            }
-            clipboard.WriteAll(data)
-            c.prop = copyPassword
-            data = ""
-        case copyUsername:
-            data := item.UserName
-            clipboard.WriteAll(data)
-            c.prop = copyUsername
-            data = ""
-        }
-        c.object = item.ObjectName
-        return c, tick
-    case tickMsg:
-        c.timer--
-        if c.timer <= 0 {
-            clipboard.WriteAll("")
-            c.timer = 10
+	switch msg := msg.(type) {
+	case ListSelectedEntry:
+		item, ok := msg.item.(BWListItem)
+		if !ok {
+			panic("Could not get BWListItem")
+		}
+		switch msg.prop {
+		case copyPassword:
+			data, err := bwm.GetPassword(item.ID)
+			if err != nil {
+				panic("Error getting Password")
+			}
+			clipboard.WriteAll(data)
+			c.prop = copyPassword
+			data = ""
+		case copyUsername:
+			data := item.UserName
+			clipboard.WriteAll(data)
+			c.prop = copyUsername
+			data = ""
+		}
+		c.object = item.ObjectName
+		c.timer = 30
+		return c, tick
+	case tickMsg:
+		c.timer--
+		if c.timer <= 0 {
+			clipboard.WriteAll("")
+			c.timer = 30
 			return c, SelectLoadingDone()
 		}
 		return c, tick
-    case tea.KeyMsg:
+	case tea.KeyMsg:
 		if msg.String() == "q" {
-            clipboard.WriteAll("")
-            return c, SelectLoadingDone()
-        }
-    }
-    var cmd tea.Cmd
-    return c, cmd
+			clipboard.WriteAll("")
+			return c, SelectLoadingDone()
+		}
+	}
+	var cmd tea.Cmd
+	return c, cmd
 }
 func (c UIClip) View() string {
 	var b strings.Builder
-    var p string
-    b.WriteString(titleStyle.Render(" Bitwarden TUI "))
-    b.WriteString("\n\n")
-    b.WriteString("Type: ")
-    switch c.prop{
-    case copyPassword:
-        p = "Password"
-    case copyUsername:
-        p = "Username"
-    }
+	var p string
+	b.WriteString(titleStyle.Render(" Bitwarden TUI "))
+	b.WriteString("\n\n")
+	b.WriteString("Type: ")
+	switch c.prop {
+	case copyPassword:
+		p = "Password"
+	case copyUsername:
+		p = "Username"
+	}
 
-    b.WriteString(focusedStyle.Render(p))
-    b.WriteString("\n")
-    b.WriteString("Object: ")
-    b.WriteString(focusedStyle.Render(c.object))
-    b.WriteString("\n\nClipboard will be cleared in " + focusedStyle.Render(fmt.Sprint(c.timer)) + " seconds.")
-    b.WriteString("\nPress " + focusedStyle.Render("q")+ " to delete Clipboard now and return to Vault.")
-    return docStyle.Render(b.String())
+	b.WriteString(focusedStyle.Render(p))
+	b.WriteString("\n")
+	b.WriteString("Object: ")
+	b.WriteString(focusedStyle.Render(c.object))
+	b.WriteString("\n\nClipboard will be cleared in " + focusedStyle.Render(fmt.Sprint(c.timer)) + " seconds.")
+	b.WriteString("\nPress " + focusedStyle.Render("q") + " to delete Clipboard now and return to Vault.")
+	return docStyle.Render(b.String())
 }
-
