e0f4d0a204a8ba5522fae85ed68f641baf5b43cd
Garrett Leber <garrett@leberra.com>
2024-08-19 18:07:04 -0400

    Fix launching on nixos/wayland, improve bindings

--- help.go
+++ help.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-    "github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/key"
 )
+
 type listKeyMap struct {
-    User  key.Binding
-    Pass  key.Binding
+	User key.Binding
+	Pass key.Binding
 }
 
 var listKeys = listKeyMap{
-    User: key.NewBinding(
-        key.WithKeys("alt+enter"),
-        key.WithHelp("Alt+Enter","Copy Username"),
-        ),
-    Pass: key.NewBinding(
-        key.WithKeys("enter"),
-        key.WithHelp("Enter","Copy Password"),
-        ),
+	User: key.NewBinding(
+		key.WithKeys("y"),
+		key.WithHelp("y", "Copy Username"),
+	),
+	Pass: key.NewBinding(
+		key.WithKeys("p"),
+		key.WithHelp("p", "Copy Password"),
+	),
 }
--- list.go
+++ list.go
@@ -1,44 +1,45 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "github.com/charmbracelet/bubbles/key"
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type ListSelectedEntry struct {
-    item list.Item
-    prop property
+	item list.Item
+	prop property
 }
 
-func SelectListSelectedEntry(id list.Item,prop property) tea.Cmd {
-    return func() tea.Msg {
-        return ListSelectedEntry{id,prop}
-    }
+func SelectListSelectedEntry(id list.Item, prop property) tea.Cmd {
+	return func() tea.Msg {
+		return ListSelectedEntry{id, prop}
+	}
 }
 
 type UIList struct {
-	list list.Model
-    selected int
+	list     list.Model
+	selected int
 }
 
-func NewUIList(h,v int) UIList{
-    d := list.NewDefaultDelegate()
-    d.Styles.SelectedTitle = d.Styles.SelectedTitle.Foreground(listSelectedStyle).BorderLeftForeground(listSelectedStyle)
-    d.Styles.SelectedDesc = d.Styles.SelectedTitle.Copy()
-    width, height := docStyle.GetFrameSize()
-    l := list.New(nil, d,h-width,v-height)
-    l.AdditionalFullHelpKeys = func() []key.Binding {
+func NewUIList(h, v int) UIList {
+	d := list.NewDefaultDelegate()
+	d.Styles.SelectedTitle = d.Styles.SelectedTitle.Foreground(listSelectedStyle).BorderLeftForeground(listSelectedStyle)
+	d.Styles.SelectedDesc = d.Styles.SelectedTitle.Copy()
+	width, height := docStyle.GetFrameSize()
+	l := list.New(nil, d, h-width, v-height)
+	l.AdditionalFullHelpKeys = func() []key.Binding {
 		return []key.Binding{
-            listKeys.User,
-            listKeys.Pass,
+			listKeys.User,
+			listKeys.Pass,
 		}
-    }
-    l.Styles.Title = titleStyle
+	}
+	l.Styles.Title = titleStyle
 
-    return UIList{list: l}
+	return UIList{list: l}
 }
 
 func (m UIList) Init() tea.Cmd {
@@ -46,31 +47,31 @@ func (m UIList) Init() tea.Cmd {
 }
 
 func (m *UIList) GetEntries() {
-    bwi,err := bwm.GetList()
-    if err != nil {
-        fmt.Println("Error: " + err.Error())
-        os.Exit(1)
-    }
-    m.list.Title = " Bitwarden Vault of: " + bwm.UserMail + " "
-    m.list.SetItems(bwi)
+	bwi, err := bwm.GetList()
+	if err != nil {
+		fmt.Println("Error: " + err.Error())
+		os.Exit(1)
+	}
+	m.list.Title = " Bitwarden Vault of: " + bwm.UserMail + " "
+	m.list.SetItems(bwi)
 }
 func (m UIList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-    case LoadingDone:
-        m.GetEntries()        
+	case LoadingDone:
+		m.GetEntries()
 	case tea.KeyMsg:
 		if msg.String() == "ctrl+c" {
 			return m, tea.Quit
 		}
-        if msg.String() == "enter" {
-			return m, SelectListSelectedEntry(m.list.SelectedItem(),copyPassword)
-        }
-        if msg.String() == "alt+enter" {
-			return m, SelectListSelectedEntry(m.list.SelectedItem(),copyUsername)
-        }
+		if msg.String() == "y" {
+			return m, SelectListSelectedEntry(m.list.SelectedItem(), copyPassword)
+		}
+		if msg.String() == "u" {
+			return m, SelectListSelectedEntry(m.list.SelectedItem(), copyUsername)
+		}
 	case tea.WindowSizeMsg:
 		m.list.SetSize(msg.Width, msg.Height)
-        return m , tea.ClearScreen
+		return m, tea.ClearScreen
 	}
 	var cmd tea.Cmd
 	m.list, cmd = m.list.Update(msg)
@@ -80,4 +81,3 @@ func (m UIList) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 func (m UIList) View() string {
 	return docStyle.Render(m.list.View())
 }
-
